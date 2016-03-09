9fd82ccbd9189ad9b44b8a56df91e53e8bd893ca
roy <roy@labsoft.cn>
2016-03-09 21:29:04 +0800

    获取广告详情的时候，增加状态显示字段

--- thunk/server/routes/getAdvertisementDetail.js
+++ thunk/server/routes/getAdvertisementDetail.js
@@ -3,12 +3,15 @@
  */
 var http = require('../utils/http'),
     stringUtils = require('../utils/string'),
-    env = require('../env').urls.getAdvertisementDetail;
+    env = require('../env').urls.getAdvertisementDetail,
+    getUserAdvertisementStatusEnv = require('../env').urls.getUserAdvertisementStatus;
 
 module.exports = {
     url : '/advertisement/:id.do',
     method : 'get',
     execute : function(req,res){
+        var result = null;
+
         var advertisementId = req.params.id;
         if(stringUtils.isBlank(advertisementId)){
             throw new Error('任务id不能为空','get_advertisement_detail');
@@ -23,7 +26,21 @@ module.exports = {
                 throw new Error(advertisementDetail.message,'get_advertisement_detail');
             }
 
-            res.writeJson(res.STATUS.SUCCESS,advertisementDetail.result,advertisementDetail.message);
+            result = advertisementDetail.result;
+
+            var statusDatas = {};
+            statusDatas[getUserAdvertisementStatusEnv.params.advertisementIds] = advertisementDetail.result.id;
+            statusDatas[getUserAdvertisementStatusEnv.params.userId] = req.userInfo.id;
+            return http.get(getUserAdvertisementStatusEnv.url,statusDatas);
+            //res.writeJson(res.STATUS.SUCCESS,advertisementDetail.result,advertisementDetail.message);
+        }).then(function(statusResponse){
+
+            if(statusResponse.status != 0){
+                throw new Error(statusResponse.message,'get_advertisement_detail');
+            }
+
+            result.status = statusResponse.result;
+            res.writeJson(res.STATUS.SUCCESS,result,'ok');
 
         });
 
