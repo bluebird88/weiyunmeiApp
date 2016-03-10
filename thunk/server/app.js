978b5041c57584c24bcdb6d7650e82da34c72369
roy <roy@labsoft.cn>
2016-03-10 10:29:45 +0800

    允许跨域请求

--- thunk/server/app.js
+++ thunk/server/app.js
@@ -44,6 +44,15 @@ app.use(function(req,res,next){
   next();
 });
 
+// 允许跨域
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  //res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  //res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
 // 签名验证
 app.use(function(req,res,next){
   var nonce_str = req.query.nonce_str;  // 随机字符串
